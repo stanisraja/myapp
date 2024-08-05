49dd4e7268c4289649cc8c0b0995f7ded2018910
stanisraja <stanis.raja@gmail.com>
2024-08-05 14:38:16 -0400

    ss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import QuizPage from './pages/quiz';
+
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/quiz" element={<QuizPage />} />
     </Routes>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Quiz from './pages/quiz';
+
 import { createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
-  },
-  {
-    path: "quiz",
-    element: <Quiz/>,
-  },
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
