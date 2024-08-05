57b998d06c75eef9e95d98c396bb616611448ffc
stanisraja <stanis.raja@gmail.com>
2024-08-05 11:17:06 -0400

    ss

---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+import {
+  Link
+} from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        1Page.click
+      </div>
+      <u1 className="navbar-menu">
+        <li> <Link to="/">Home</Link> </li>
+        <li> <Link to="/quiz">Quiz</Link> </li>
+      </u1>
+    </div>  
+  )
+}
+
+export default Navbar;
