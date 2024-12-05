7c43157c9b40db76aa22c1b690d16b1077cc7842
stanisraja <stanis.raja@gmail.com>
2024-12-05 18:49:54 -0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -23,6 +23,30 @@ const Home = () => {
         </Link> 
       </div>
     </div>
+    <link rel="stylesheet" href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css">
+    <script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+    <df-messenger
+      project-id="jollyinfo"
+      agent-id="0fdb1b9c-80a9-40da-9ac5-f95d48e93833"
+      language-code="en"
+      max-query-length="-1">
+      <df-messenger-chat-bubble
+       chat-title="Jolly">
+      </df-messenger-chat-bubble>
+    </df-messenger>
+    <style>
+      df-messenger {
+        z-index: 999;
+        position: fixed;
+        --df-messenger-font-color: #000;
+        --df-messenger-font-family: Google Sans;
+        --df-messenger-chat-background: #f3f6fc;
+        --df-messenger-message-user-background: #d3e3fd;
+        --df-messenger-message-bot-background: #fff;
+        bottom: 16px;
+        right: 16px;
+      }
+    </style>
   );
   
 };
