995ddaf1885abca7511779d8869f21122698ac33
stanisraja <stanis.raja@gmail.com>
2024-12-06 18:13:47 -0500

    inside box

--- src/pages/home.js
+++ src/pages/home.js
@@ -26,45 +26,45 @@ const Home = () => {
             FafTube
           </button>
         </Link>
+
+        {/* Add Dialogflow Messenger */}
+        <link
+          rel="stylesheet"
+          href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css"
+        />
+        <script
+          src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"
+          async
+        ></script>
+        <df-messenger
+          project-id="jollyinfo"
+          agent-id="0fdb1b9c-80a9-40da-9ac5-f95d48e93833"
+          language-code="en"
+          max-query-length="-1"
+        >
+          <df-messenger-chat-bubble chat-title="Jolly"></df-messenger-chat-bubble>
+        </df-messenger>
+
+        {/* Inline Styling for Messenger */}
+        <style>
+          {`
+            df-messenger {
+              z-index: 9999;
+              position: fixed;
+              --df-messenger-font-color: #000;
+              --df-messenger-font-family: Google Sans;
+              --df-messenger-chat-background: #f3f6fc;
+              --df-messenger-message-user-background: #d3e3fd;
+              --df-messenger-message-bot-background: #fff;
+              bottom: 16px;
+              right: 16px;
+              display: block;
+              width: 400px; /* Adjust as needed */
+              height: 600px; /* Adjust as needed */
+            }
+          `}
+        </style>
       </div>
-
-      {/* Add Dialogflow Messenger */}
-      <link
-        rel="stylesheet"
-        href="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/themes/df-messenger-default.css"
-      />
-      <script
-        src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"
-        async
-      ></script>
-      <df-messenger
-        project-id="jollyinfo"
-        agent-id="0fdb1b9c-80a9-40da-9ac5-f95d48e93833"
-        language-code="en"
-        max-query-length="-1"
-      >
-        <df-messenger-chat-bubble chat-title="Jolly"></df-messenger-chat-bubble>
-      </df-messenger>
-
-      {/* Inline Styling for Messenger */}
-      <style>
-        {`
-          df-messenger {
-            z-index: 9999;
-            position: fixed;
-            --df-messenger-font-color: #000;
-            --df-messenger-font-family: Google Sans;
-            --df-messenger-chat-background: #f3f6fc;
-            --df-messenger-message-user-background: #d3e3fd;
-            --df-messenger-message-bot-background: #fff;
-            bottom: 16px;
-            right: 16px;
-            display: block;
-            width: 400px; /* Adjust as needed */
-            height: 600px; /* Adjust as needed */
-          }
-        `}
-      </style>
     </div>
   );
 };
